f9012a316f821a7c2b490d0963ea4d9550a02134
srijantrpth <srijantrpth@gmail.com>
2024-11-09 12:31:24 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState,useCallback,useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+    </>
+  );
+}
+
+export default App;
